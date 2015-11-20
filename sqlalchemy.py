471e9fe4eed579a50aef80124b0a6ea3fe6c165b
Юрий Пайков <paykov@findmeals.com>
2015-11-20 16:06:40 +0500

    Snippet for VALUES() clause support for SQLAlchemy

--- sqlalchemy.py
+++ sqlalchemy.py
@@ -1,3 +1,15 @@
+import functools
+
+from collections import Iterable
+from warnings import warn
+
+from sqlalchemy.dialects.postgresql import array
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import FromClause, ClauseElement, bindparam
+from sqlalchemy.sql.sqltypes import NULLTYPE
+
+
 class class_with_session:
     def __init__(self, func):
 
@@ -5,7 +17,7 @@ class class_with_session:
         self.on_exception_handler = None
 
         def wrapper(*args, **kw):
-            session = get_sa_session()
+            session = Session()
             if session.transaction is None:
                 session.begin()
             try:
@@ -24,6 +36,7 @@ class class_with_session:
                         return handler_res
                 else:
                     raise
+
         self.func = wrapper
 
     def __get__(self, instance, class_):
@@ -52,9 +65,63 @@ class class_with_session:
 
         return deco
 
+
 def with_session(fn):
     '''
     Начинает транзакцию, если ещё не начата
     Если в функции происходит исключение(по умолчанию - любое), откатывает её
     '''
     return class_with_session(fn)
+
+
+class values(FromClause):
+    named_with_column = True
+
+    def __init__(self, columns, *args, **kw):
+        self._column_args = columns
+        self.list = args
+        self.alias_name = self.name = kw.pop('alias_name', None)
+
+    def _populate_column_collection(self):
+        # self._columns.update((col.name, col) for col in self._column_args)
+        for c in self._column_args:
+            c._make_proxy(self, c.name)
+
+
+@compiles(values)
+def compile_values(clause, compiler, asfrom=False, **kw):
+    def decide(value, column):
+        add_type_hint = False
+        if isinstance(value, array) and not value.clauses:  # for empty array literals
+            add_type_hint = True
+
+        if isinstance(value, ClauseElement):
+            intermediate = compiler.process(value)
+            if add_type_hint:
+                intermediate += '::' + str(column.type)
+            return intermediate
+
+        else:
+            intermediate = compiler.render_literal_value(
+                value,
+                column.type if (value is not None) else NULLTYPE
+            )
+            if value is not None:
+                intermediate = compiler.process(
+                    bindparam(None, value=intermediate)
+                ) + '::' + str(column.type)
+            return intermediate
+
+    columns = clause.columns
+    v = "VALUES %s" % ", ".join(
+        "(%s)" % ", ".join(
+            decide(elem, column)
+            for elem, column in zip(tup, columns))
+        for tup in clause.list
+    )
+    if asfrom:
+        if clause.alias_name:
+            v = "(%s) AS %s (%s)" % (v, clause.alias_name, (", ".join(c.name for c in clause.columns)))
+        else:
+            v = "(%s)" % v
+    return v
