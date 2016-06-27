5288025dd511e2b65697e19be11253bf5724e271
Юрий Пайков <paykov@findmeals.com>
2016-06-27 19:58:09 +0500

    Added issue creation feature

--- issue_hook.py
+++ issue_hook.py
@@ -7,6 +7,8 @@ import argh
 from argh.assembling import set_default_command
 import re
 import requests
+import sys
+import os
 from requests.exceptions import HTTPError
 
 __author__ = 'crystal'
@@ -42,6 +44,62 @@ class JiraConnection:
         )
         return resp.json()
 
+    def get_user_by_email(self, email, expand='all'):
+        resp = self.make_request(
+            action='user/search',
+            params={'username': email}
+        )
+        return resp.json()
+
+    def create_issue(self, project_key, summary, description, issue_type, components, expand='all'):
+        author = self.get_user_by_email(
+            os.getenv('GIT_AUTHOR_EMAIL')
+        )[0]['name']
+
+        resp = self.make_request(
+            action='issue/',
+            params={'expand': expand},
+            json_body={
+                'fields': {
+                    'project': {
+                        'key': project_key
+                    },
+                    'issuetype': {
+                        'id': issue_type
+                    },
+                    "assignee": {
+                        "name": author
+                    },
+                    "reporter": {
+                        "name": author
+                    },
+                    'summary': summary,
+                    'description': description,
+                    'components': [
+                        {'id': id_}
+                        for id_ in components
+                    ]
+                }
+            },
+            verb='POST'
+        )
+        return resp.json()
+
+    def get_project_components(self, project_key, expand='all'):
+        resp = self.make_request(
+            action='project/%s/components' % project_key,
+            params={'expand': expand}
+        )
+        return resp.json()
+
+    def get_issue_types(self, expand='all'):
+        resp = self.make_request(
+            action='issuetype',
+            params={'expand': expand}
+        )
+        return resp.json()
+
+
     def make_request(self, action, params=None, json_body=None, verb='GET', apiname='api', ):
         try:
             url = self.template.format(action=action, api=apiname, **self.__dict__)
@@ -53,11 +111,13 @@ class JiraConnection:
                 json=json_body,
                 verify=False,
                 headers={
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json, text/plain;q=0.7',  # Show errors in plain text or json, not XML
                 },
                 cookies=self.session.cookies
             )
             if str(resp.status_code).startswith('4') :
+                print(resp.text)
                 raise HTTPError(resp.status_code)
         except HTTPError as e:
             print('%s occurred while processing %s ' % (e, action))
@@ -77,8 +137,49 @@ def main(args):
     resp = connobj.make_request(action='field').json()
     epic_link_field_id = ([x['id'] for x in resp if x['name'] == 'Epic Link'] or [None])[0]
 
+    def create_issue(matchobj, orig_text, regexp):
+        project_key = matchobj.group('project')
+
+        issue_type_name = (re.findall(r'#(bug|feature)', orig_text.lower()) + ['bug'])[0]
+        issue_type = [
+            issue_type['id'] for issue_type
+            in connobj.get_issue_types()
+            if issue_type_name in issue_type['name'].lower()
+        ][0]
+
+        if '\n\n' in orig_text:
+            summary, description = orig_text.split('\n\n', maxsplit=1)
+        else:
+            summary, description = orig_text, ''
+        summary = regexp.sub('', summary)  # Header text without control tokens
+        summary = re.sub(r'#(bug|feature)', '', summary)
+
+        components_list = sorted(
+            connobj.get_project_components(project_key),
+            key=lambda x: x['name']
+        )
+
+        print('Enter one of the components below:')
+        for idx, component in enumerate(components_list):
+            print('%d. %s' % (idx, component['name']))
+        component_idx = int(sys.stdin.readline())
+        components = ' Компоненты: ' + components_list[component_idx]['name']
+
+
+
+        result = connobj.create_issue(
+            project_key,
+            summary,
+            description,
+            issue_type,
+            components=[components_list[component_idx]['id']]
+        )
+        print('Ссылка: %s' % result['self'])
+        return 'refs #%s (%s)%s' % (result['key'], summary, components)
+
     def replace_func(orig_text, matchobj):
         key = matchobj.group(1)
+
         try:
             info = connobj.get_issue_info(key)
         except HTTPError:
@@ -108,11 +209,18 @@ def main(args):
                 )
 
     with open(args.commit_msg_file, 'r+') as f:
-        text = ''.join(f.readlines())
+        text = ''.join(f.readlines()).strip()
         f.seek(0)
         f.truncate()
-        text = re.sub(r'refs\s*#(\w+-\d+)', lambda x: replace_func(text, x), text)
-        f.write(text)
+
+        regexp = re.compile(r'\s*refs #newissue\s+(?P<project>\w+)(?P<parent>-\d+)?\s*')
+        matchobj = regexp.search(text)
+        if matchobj:
+            text = create_issue(matchobj, text, regexp=regexp)
+
+        else:
+            text = re.sub(text, lambda x: replace_func(text, x), text)
+        f.write(text+'\n')
 
 
 if __name__ == '__main__':
